22b74801a0083bd122679081a865ffb3f688ca68
Vegard Bendiktsen <glitchdetector@gmail.com>
2019-11-09 16:38:28 +0100

    Added convar editor support

--- wap-chatlog/__resource.lua
+++ wap-chatlog/__resource.lua
@@ -8,4 +8,19 @@ description "A chatlog plugin for Webadmin"
 dependency 'webadmin-lua'
 webadmin_plugin 'yes'
 
+webadmin_settings 'yes'
+convar_category 'Chat History' {
+    'Configure the Chat History interface',
+    {
+        {'Limit', 'chatlog_limit', 'CV_MULTI', {
+            {"100", 100},
+            {"200", 200},
+            {"500", 500},
+            {"Unlimited", -1},
+        }},
+        {'Per Page', 'chatlog_per_page', 'CV_SLIDER', 25, 10, 50},
+        {'Unread Indicator', 'chatlog_show_unread_messages', 'CV_BOOL', true, 'Show the amount of unread messages'},
+    }
+}
+
 server_script 'chatlog.lua'
